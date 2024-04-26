837aa1a066bbcc1a67ca8f4829dc9b8346130456
pciruelos <pciruelos@yahoo.com>
2024-04-26 13:05:06 +0200

    delete and edit method.

--- static/main.js
+++ static/main.js
@@ -1,6 +1,8 @@
 const userForm = document.querySelector("#userForm");
 
 let users = [];
+let editing = false;
+let userId = null;
 
 window.addEventListener("DOMContentLoaded", async () => {
   const res = await fetch("/api/users");
@@ -15,20 +17,41 @@ userForm.addEventListener("submit", async (e) => {
   const email = userForm["email"].value;
   const password = userForm["password"].value;
 
-  const response = await fetch("/api/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      email,
-      password,
-    }),
-  });
-  const data = await response.json();
-  users.unshift(data);
-  renderUser(users)
+  if (!editing) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+    users.unshift(data);
+  } else {
+    const response = await fetch(`/api/users/${userId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const updatedUser = await response.json();
+
+    users = users.map((u) => (u.id === updatedUser.id ? updatedUser : u));
+    editing = false;
+    userId = null;
+  }
+
+  renderUser(users);
   userForm.reset();
 });
 
@@ -43,12 +66,32 @@ function renderUser(users) {
     userItem.innerHTML = `<header class="d-flex justify-content-between align-items-center">
     <h3>${u.username}</h3>
     <div>
-        <button class="btn btn-danger btn-sm">Delete</button>
-        <button class="btn btn-warning btn-sm">Edit</button>
+        <button class="btn-delete btn btn-danger btn-sm">Delete</button>
+        <button class="btn-edit btn btn-warning btn-sm">Edit</button>
     </div>
     </header>
     <p>${u.email}</p>
     <p class="text-truncate">${u.password}</p>`;
+
+    const btnDelete = userItem.querySelector(".btn-delete");
+    btnDelete.addEventListener("click", async () => {
+      const response = await fetch(`/api/users/${u.id}`, {
+        method: "DELETE",
+      });
+      const data = await response.json();
+      users = users.filter((u) => u.id !== data.id);
+      renderUser(users);
+    });
+
+    const btnEdit = userItem.querySelector(".btn-edit");
+    btnEdit.addEventListener("click", async () => {
+      const response = await fetch(`/api/users/${u.id}`);
+      const data = await response.json();
+      userForm["username"].value = data.username;
+      userForm["email"].value = data.email;
+      editing = true;
+      userId = data.id;
+    });
 
     userList.append(userItem);
   });
